d253bdeccda0c13ccd536cc793fce3f7641f70d0
Braulio Cassule <brauliocassule94@gmail.com>
2017-12-17 15:56:12 +0100

    commit settings changes

--- app/src/main/java/com/codingdojoangola/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/codingdojoangola/ui/settings/SettingsActivity.java
@@ -297,7 +297,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
-            bindPreferenceSummaryToValue(findPreference("example_text"));
+            //bindPreferenceSummaryToValue(findPreference("example_text"));
             bindPreferenceSummaryToValue(findPreference("pref_key_conta"));
         }
 
