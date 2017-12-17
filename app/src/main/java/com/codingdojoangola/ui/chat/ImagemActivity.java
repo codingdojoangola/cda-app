2fdc0d0d66b3a8362a1312c5424ee73ad28fa292
JADeveloper <jonasapolo1@gmail.com>
2017-12-17 12:30:54 +0100

    Listagem de membros na sala do chat, iniciar conversa, anexar e visualisar imagem

--- app/src/main/java/com/codingdojoangola/ui/chat/ImagemActivity.java
+++ app/src/main/java/com/codingdojoangola/ui/chat/ImagemActivity.java
@@ -216,35 +216,5 @@ public class ImagemActivity extends AppCompatActivity implements View.OnTouchLis
         float y = event.getY(0) + event.getY(1);
         point.set(x / 2, y / 2);
     }
-
-    /* Show an event in the LogCat view, for debugging */
-//    private void dumpEvent(MotionEvent event)
-//    {
-//        String names[] = { "DOWN", "UP", "MOVE", "CANCEL", "OUTSIDE","POINTER_DOWN", "POINTER_UP", "7?", "8?", "9?" };
-//        StringBuilder sb = new StringBuilder();
-//        int action = event.getAction();
-//        int actionCode = action & MotionEvent.ACTION_MASK;
-//        sb.append("event ACTION_").append(names[actionCode]);
-//
-//        if (actionCode == MotionEvent.ACTION_POINTER_DOWN || actionCode == MotionEvent.ACTION_POINTER_UP)
-//        {
-//            sb.append("(pid ").append(action >> MotionEvent.ACTION_POINTER_INDEX_SHIFT); //ACTION_POINTER_ID_SHIFT
-//
-//            sb.append(")");
-//        }
-//
-//        sb.append("[");
-//        for (int i = 0; i < event.getPointerCount(); i++)
-//        {
-//            sb.append("#").append(i);
-//            sb.append("(pid ").append(event.getPointerId(i));
-//            sb.append(")=").append((int) event.getX(i));
-//            sb.append(",").append((int) event.getY(i));
-//            if (i + 1 < event.getPointerCount())
-//                sb.append(";");
-//        }
-//
-//        sb.append("]");
-//        Log.d("Touch Events ---------", sb.toString());
-//    }
+    
 }
